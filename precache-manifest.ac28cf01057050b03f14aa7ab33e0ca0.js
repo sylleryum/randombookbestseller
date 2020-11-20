1af34eb45c231216dd96ba3fd98b9b8637240710
sylleryum <alisson_piuco@yahoo.com.br>
2020-11-20 12:18:30 -0300

    Updates

---
+++ precache-manifest.ac28cf01057050b03f14aa7ab33e0ca0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f891094f78a2eeef6c6d1c3cfa705244",
+    "url": "/randombookbestseller/index.html"
+  },
+  {
+    "revision": "b22aeb1fd0d15e3ba354",
+    "url": "/randombookbestseller/static/css/2.d97600f8.chunk.css"
+  },
+  {
+    "revision": "679ab65577fc3d1f6705",
+    "url": "/randombookbestseller/static/css/main.ef103ac2.chunk.css"
+  },
+  {
+    "revision": "b22aeb1fd0d15e3ba354",
+    "url": "/randombookbestseller/static/js/2.ce664fb5.chunk.js"
+  },
+  {
+    "revision": "41a6866a1b5bd1f04a40d009dc8f6e80",
+    "url": "/randombookbestseller/static/js/2.ce664fb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "679ab65577fc3d1f6705",
+    "url": "/randombookbestseller/static/js/main.1f9d58b2.chunk.js"
+  },
+  {
+    "revision": "f43d102b52d3a8f7d3aa",
+    "url": "/randombookbestseller/static/js/runtime-main.d29daeab.js"
+  }
+]);
